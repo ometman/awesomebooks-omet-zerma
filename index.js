05b1a3a2560a8e91197c6a019d3d20cf71963c6a
ometman <revomet@gmail.com>
2023-08-07 12:29:41 +0000

    Add function to add new book, store in local storage

--- index.js
+++ index.js
@@ -1,12 +1,30 @@
-const form = document.querySelector('#form');
-const title = document.getElementById('title');
-const author = document.getElementById('author');
+const form = document.querySelector('#new-bk');
+const title = document.getElementById('bk-title');
+const author = document.getElementById('bk-author');
 const bookList = document.getElementById('show-bks');
 let collection = JSON.parse(localStorage.getItem('collection')) || [];
-let book = [{
-  title: 'Alpen',
-  author: 'Zalpen',
-  id: 1234574,
-}];
+// let book = [{
+//   title: 'Alpen',
+//   author: 'Zalpen',
+//   id: 1234574,
+// }];
 
+function newBook() {
+  const book = {
+    title: title.value,
+    author: author.value,
+    idNumber: Math.floor(Math.random() * 1000000),
+  };
+  collection.push(book);
+  localStorage.setItem('collection', JSON.stringify(collection));
+}
 
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+  if (title.value !== '' && author.value !== '') {
+    newBook();
+    form.reset();
+  } else {
+    alert('Enter valid values for title and author fields, please.');
+  }
+});
