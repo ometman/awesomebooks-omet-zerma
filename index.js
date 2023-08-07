e4a942e9119a64a9248e2d1e8bba0880baf9246a
ometman <revomet@gmail.com>
2023-08-07 12:43:03 +0000

    Add function to show books

--- index.js
+++ index.js
@@ -24,10 +24,28 @@ function deleteBook(idNumber) {
   localStorage.setItem('collection', JSON.stringify(collection));
 }
 
+function printCollection(book) {
+  const tableRow = document.createElement('tr');
+  const newTitle = document.createElement('td');
+  const newAuthor = document.createElement('td');
+  const deleteButton = document.createElement('button');
+  newTitle.innerText = book.title;
+  newAuthor.innerText = book.author;
+  deleteButton.innerHTML = 'Delete';
+  tableRow.append(newTitle, newAuthor, deleteButton);
+  bookList.append(tableRow);
+  deleteButton.addEventListener('click', () => {
+    deleteButton.parentElement.remove();
+    deleteBook(book.idNumber);
+  });
+}
+collection.forEach(printCollection);
+
 form.addEventListener('submit', (event) => {
   event.preventDefault();
   if (title.value !== '' && author.value !== '') {
     newBook();
+    printCollection(book);
     form.reset();
   } else {
     alert('Enter valid values for title and author fields, please.');
