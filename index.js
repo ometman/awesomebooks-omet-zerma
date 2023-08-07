28050788bf6260f2e375186b2f484491759fc792
Zermarden <zermarden@gmail.com>
2023-08-07 18:10:37 +0600

    Change variable to the proper one - show-bks

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const form = document.querySelector('#form');
 const title = document.getElementById('title');
 const author = document.getElementById('author');
-const bookList = document.getElementById('books-list');
+const bookList = document.getElementById('show-bks');
 let collection = JSON.parse(localStorage.getItem('collection')) || [];
 let book = [{
   title: 'Alpen',
