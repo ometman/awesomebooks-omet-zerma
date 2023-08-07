f23bbd58ef7fc30ac09bf9d6cefebaea7c2a02d8
Zermarden <zermarden@gmail.com>
2023-08-07 18:06:16 +0600

    Create a collection that keeps a list of books.

---
+++ index.js
@@ -0,0 +1,12 @@
+const form = document.querySelector('#form');
+const title = document.getElementById('title');
+const author = document.getElementById('author');
+const bookList = document.getElementById('books-list');
+let collection = JSON.parse(localStorage.getItem('collection')) || [];
+let book = [{
+  title: 'Alpen',
+  author: 'Zalpen',
+  id: 1234574,
+}];
+
+
