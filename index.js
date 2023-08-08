34ef4f638c5c00bfa18b6d22f6bce11c0e29c7b0
ometman <revomet@gmail.com>
2023-08-08 09:39:00 +0000

    Add book, storage, add book and delete book classes

--- index.js
+++ index.js
@@ -1,50 +1,38 @@
-const form = document.querySelector('#new-bk');
-const title = document.getElementById('bk-title');
-const author = document.getElementById('bk-author');
-const bookList = document.getElementById('show-bks');
-let collection = JSON.parse(localStorage.getItem('collection')) || [];
-let book = {};
-
-function newBook() {
-  book = {
-    title: title.value,
-    author: author.value,
-    idNumber: Math.floor(Math.random() * 1000000),
-  };
-  collection.push(book);
-  localStorage.setItem('collection', JSON.stringify(collection));
-}
-
-function deleteBook(idNumber) {
-  collection = collection.filter((books) => books.idNumber !== idNumber);
-  localStorage.setItem('collection', JSON.stringify(collection));
-}
-
-function printCollection(book) {
-  const tableRow = document.createElement('tr');
-  const newTitle = document.createElement('td');
-  const newAuthor = document.createElement('td');
-  const deleteButton = document.createElement('button');
-  newTitle.innerText = book.title;
-  newAuthor.innerText = book.author;
-  deleteButton.innerHTML = 'Delete';
-  tableRow.append(newTitle, newAuthor, deleteButton);
-  bookList.append(tableRow);
-  deleteButton.addEventListener('click', () => {
-    deleteButton.parentElement.remove();
-    deleteBook(book.idNumber);
-  });
-}
-
-collection.forEach(printCollection);
-
-form.addEventListener('submit', (event) => {
-  event.preventDefault();
-  if (title.value !== '' && author.value !== '') {
-    newBook();
-    printCollection(book);
-    form.reset();
-  } else {
-    alert('Enter valid values for title and author fields, please.');
+/* eslint-disable max-classes-per-file */
+class Book {
+  constructor(title, author) {
+    this.title = title;
+    this.author = author;
   }
-});
+}
+
+class Storage {
+  static getBooks() {
+    let collection;
+    if (localStorage.getItem('collection') === null) {
+      collection = [];
+    } else {
+      collection = JSON.parse(localStorage.getItem('collection'));
+    }
+
+    return collection;
+  }
+
+  static newBook(book) {
+    const collection = Storage.getBooks();
+    collection.push(book);
+    localStorage.setItem('collection', JSON.stringify(collection));
+  }
+
+  static delBook(author) {
+    const collection = Storage.getBooks();
+
+    collection.forEach((book, index) => {
+      if (book.author === author) {
+        collection.splice(index, 1);
+      }
+    });
+
+    localStorage.setItem('collection', JSON.stringify(collection));
+  }
+}
