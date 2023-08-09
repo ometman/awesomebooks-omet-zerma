ec0ee23e518faaae1c88b5ad8674b1d0eb1266cc
ometman <revomet@gmail.com>
2023-08-09 10:53:17 +0000

    Add script for content change

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 /* eslint-disable no-use-before-define */
 import { mainHeader } from './modules/header.js';
 import pageSwitch from './modules/contentSwitch.js';
-import { theFooter } from './modules/footer.js';
-import { addNewBook } from './modules/addbook-event.js';
-import { currentDate } from './modules/getCurrrentDate.js';
+// import { theFooter } from './modules/footer.js';
+// import { addNewBook } from './modules/addbook-event.js';
+import { currentDate } from './modules/getCurrentDate.js';
 
 window.onload = () => {
   mainHeader.style.display = 'block';
   pageSwitch();
-  theFooter.style.display = 'block';
+  // theFooter.style.display = 'block';
   currentDate();
-  addNewBook();
+  // addNewBook();
 };
