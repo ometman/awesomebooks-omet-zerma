ba3800d78e0df80a545ad39bd14e2958be68c5b0
ometman <revomet@gmail.com>
2023-08-09 10:31:35 +0000

    Add modules for navigation

--- index.js
+++ index.js
@@ -1,99 +1,14 @@
-/* eslint-disable max-classes-per-file */
-class Book {
-  constructor(title, author) {
-    this.title = title;
-    this.author = author;
-  }
-}
+/* eslint-disable no-use-before-define */
+import { mainHeader } from './modules/header.js';
+import pageSwitch from './modules/contentSwitch.js';
+import { theFooter } from './modules/footer.js';
+import { addNewBook } from './modules/addbook-event.js';
+import { currentDate } from './modules/getCurrrentDate.js';
 
-class Storage {
-  static getBooks() {
-    let collection;
-    if (localStorage.getItem('collection') === null) {
-      collection = [];
-    } else {
-      collection = JSON.parse(localStorage.getItem('collection'));
-    }
-
-    return collection;
-  }
-
-  static newBook(book) {
-    const collection = Storage.getBooks();
-    collection.push(book);
-    localStorage.setItem('collection', JSON.stringify(collection));
-  }
-
-  static delBook(author) {
-    const collection = Storage.getBooks();
-
-    collection.forEach((book, index) => {
-      if (book.author === author) {
-        collection.splice(index, 1);
-      }
-    });
-
-    localStorage.setItem('collection', JSON.stringify(collection));
-  }
-}
-
-class Display {
-  static showBooks() {
-    const collection = Storage.getBooks();
-
-    collection.forEach((book) => Display.printCollection(book));
-  }
-
-  static printCollection(book) {
-    const bookList = document.querySelector('#show-bks');
-
-    const tableRow = document.createElement('tr');
-    const dataCell = document.createElement('td');
-    const dataCell2 = document.createElement('td');
-    const button = document.createElement('button');
-    dataCell.innerHTML = ` <span>"${book.title}"</span> by <span>${book.author}</span> `;
-    tableRow.append(dataCell);
-    tableRow.append(dataCell2);
-    dataCell2.append(button);
-    button.classList.add('delete');
-    button.innerText = 'Remove';
-    bookList.appendChild(tableRow);
-  }
-
-  static deleteBook(el) {
-    if (el.classList.contains('delete')) {
-      el.parentElement.parentElement.remove();
-    }
-  }
-
-  static clearFields() {
-    document.querySelector('#bk-title').value = '';
-    document.querySelector('#bk-author').value = '';
-  }
-}
-
-document.addEventListener('DOMContentLoaded', Display.showBooks);
-
-document.querySelector('#new-bk').addEventListener('submit', (e) => {
-  e.preventDefault();
-  const title = document.querySelector('#bk-title').value;
-  const author = document.querySelector('#bk-author').value;
-
-  if (title === '' || author === '') {
-    Display.showAlert('Enter valid values for title and author fields, please.');
-  } else {
-    const book = new Book(title, author);
-
-    Display.printCollection(book);
-
-    Storage.newBook(book);
-
-    Display.clearFields();
-  }
-});
-
-document.querySelector('#show-bks').addEventListener('click', (e) => {
-  Display.deleteBook(e.target);
-
-  Storage.delBook(e.target.parentElement.previousElementSibling.lastElementChild.textContent);
-});
+window.onload = () => {
+  mainHeader.style.display = 'block';
+  pageSwitch();
+  theFooter.style.display = 'block';
+  currentDate();
+  addNewBook();
+};
